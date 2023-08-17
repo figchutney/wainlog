159a45af1d7963e9724d6cdbf9af0dcadbf00d93
Ben Watson <52138939+figchutney@users.noreply.github.com>
2023-08-17 16:11:03 +0100

    feat: implement Google OAuth for login

--- wainlog/adapters/api/views.py
+++ wainlog/adapters/api/views.py
@@ -1,4 +1,19 @@
-from flask import Blueprint, flash, g, redirect, render_template, url_for
+import secrets
+from urllib.parse import urlencode
+
+import requests
+from flask import (
+    Blueprint,
+    abort,
+    current_app,
+    flash,
+    g,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+)
 from flask_login import current_user, login_user, logout_user
 from werkzeug import Response
 
@@ -60,50 +75,88 @@ def root() -> str | Response:
     )
 
 
-@bp.route("/login", methods=["GET", "POST"])
-def login() -> Response | str:
-    if current_user.is_authenticated:
+@bp.route("/authorize/<provider>", methods=["GET"])
+def oauth2_authorize(provider: str) -> Response:
+    if not current_user.is_anonymous:
         return redirect(url_for("wainlog.root"))
 
-    form = forms.Login()
+    session["oauth2_state"] = secrets.token_urlsafe(16)
 
-    if form.validate_on_submit():
-        user = persister.get_user_db_from_username(
-            session=g.session, username=form.username.data
-        )
+    query_string = urlencode(
+        {
+            "client_id": current_app.config["GOOGLE_OAUTH_CLIENT_ID"],
+            "redirect_uri": url_for(
+                "wainlog.oauth2_callback", provider=provider, _external=True
+            ),
+            "response_type": "code",
+            "scope": " ".join(current_app.config["GOOGLE_OAUTH_SCOPES"]),
+            "state": session["oauth2_state"],
+        }
+    )
 
-        if user is None or user.check_password(form.password.data) is False:
-            flash("Invalid username or password")
-            return redirect(url_for("wainlog.login"))
-        login_user(user)
+    return redirect(
+        current_app.config["GOOGLE_OAUTH_AUTHORIZE_URL"] + "?" + query_string
+    )
 
+
+@bp.route("/callback/<provider>", methods=["GET"])
+def oauth2_callback(provider: str) -> Response:  # noqa: C901
+    if not current_user.is_anonymous:
         return redirect(url_for("wainlog.root"))
 
-    return render_template("login.html", form=form)
+    if "error" in request.args:
+        for k, v in request.args.items():
+            if k.startswith("error"):
+                flash(f"{k}: {v}")
+        return redirect(url_for("index"))
+
+    if request.args["state"] != session.get("oauth2_state"):
+        abort(401)
+
+    if "code" not in request.args:
+        abort(401)
+
+    response = requests.post(
+        current_app.config["GOOGLE_OAUTH_TOKEN_URL"],
+        data={
+            "client_id": current_app.config["GOOGLE_OAUTH_CLIENT_ID"],
+            "client_secret": current_app.config["GOOGLE_OAUTH_CLIENT_SECRET"],
+            "code": request.args["code"],
+            "grant_type": "authorization_code",
+            "redirect_uri": url_for(
+                "wainlog.oauth2_callback", provider=provider, _external=True
+            ),
+        },
+        headers={"Accept": "application/json"},
+    )
+    if response.status_code != 200:
+        abort(401)
+    oauth2_token = response.json().get("access_token")
+    if not oauth2_token:
+        abort(401)
+
+    response = requests.get(
+        current_app.config["GOOGLE_OAUTH_USERINFO_URL"],
+        headers={
+            "Authorization": "Bearer " + oauth2_token,
+            "Accept": "application/json",
+        },
+    )
+    if response.status_code != 200:
+        abort(401)
+    email = response.json()["email"]
+
+    user = persister.get_user_db_from_email(g.session, email)
+    if user is None:
+        user = model.User(email=email, username=email.split("@")[0])
+        persister.add_user(g.session, user)
+        g.session.commit()
+
+    login_user(user)
+    return redirect(url_for("wainlog.root"))
 
 
 @bp.route("/logout")
 def logout() -> Response:
     logout_user()
-
     return redirect(url_for("wainlog.root"))
-
-
-@bp.route("/register", methods=["GET", "POST"])
-def register() -> Response | str:
-    if current_user.is_authenticated:
-        return redirect(url_for("root"))
-
-    form = forms.Register()
-
-    if form.validate_on_submit():
-        user = model.User(
-            username=form.username.data,
-            email=form.email.data,
-        )
-        user.set_password(form.password.data)
-        persister.add_user(session=g.session, user=user)
-        g.session.commit()
-        return redirect(url_for("wainlog.login"))
-
-    return render_template("register.html", title="Register", form=form)
